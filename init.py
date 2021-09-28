1ee24a4ad7fb890efb2a08fa6ae43e10d428c90a
FatSquare <legitcsgo5@gmail.com>
2021-09-28 08:54:03 -0400

    added base.html + first certifcate 😏

--- init.py
+++ init.py
@@ -1,73 +1,36 @@
-from flask import Flask,render_template,request
-#from flask_mysqldb import MySQL
-#import private
+from flask import Flask,redirect,render_template,request,url_for
 from colorama import Fore
-
+import flaskpdf
 port = 5000
 
 app = Flask(__name__)
-#app.config['MYSQL_HOST'] = private.sql_host
-#app.config['MYSQL_USER'] = private.sql_username
-#app.config['MYSQL_PASSWORD'] = private.sql_password
-#app.config['MYSQL_DB'] = private.sql_dbname
-
-###mysql = MySQL(app)
-
-def GetViews():
-    return 0
-#    query = f"select count(*) from {private.sql_tablename}"
-#    cur = mysql.connection.cursor()
-#    cur.execute(query)
-#    mysql.connection.commit()
-#    data = cur.fetchall()
-#    cur.close()
-#    return data[0][0]
-#
-
-def AddView(ipaddr):
-    return 0
-    #cur = mysql.connection.cursor()
-    #try: 
-    #    cur.execute(f"INSERT INTO {private.sql_tablename} ({private.sql_columnname}) VALUES(%s)",[private.hideIP(ipaddr)])
-    #    mysql.connection.commit()
-    #except Exception as ex:
-    #    print(ex)
-    #    pass
-    #cur.close()
-    #print(Fore.GREEN + f"Incoming connection from: {ipaddr}" +Fore.WHITE)
+flaskpdf.init_app(app)
 
 @app.route('/')
 def Home():
-    AddView(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-    return render_template('index.html',views=GetViews())
+    return render_template('index.html')
 
 @app.route('/certificates/')
 def Certs():
-    AddView(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-    return render_template('certs.html',views=GetViews())
+    return render_template('certs.html')
 
 @app.route('/certificates/<certname>')
 def ShowCert(certname):
-    AddView(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     if certname == "jusoor":
-        imgurl = "https://www.simplilearn.com/ice9/certificates/MIT-SCC_certificate.jpg"
-        return f'<img src="{imgurl}" width=50% height=50% style="position:relative;left:25%;top:25%"/>'
+        return  redirect(url_for('static',filename='certs/Ayoub-Hedri-certificate.pdf'))
     return 'Invalid certificate name!'
 
 @app.route('/contact/')
 def Contact():
-    AddView(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-    return render_template('contact.html',views=GetViews())
+    return render_template('contact.html')
 
 @app.route('/hack/')
 def HackSquare():
-    AddView(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-    return render_template('hack.html',views=GetViews())
+    return render_template('hack.html')
 
 
 @app.route('/flag/')
 def GetFlag():
-    AddView(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     return 'U3F1YXIze1doNHRfNF9XMzFyZDB9'
 
 
@@ -77,7 +40,7 @@ for i in (500,404,403):
         ercode = str(er)[0:3]
         return render_template('error.html',errorCode=ercode)
 
-if __name__ == "__main__" and False: # Change True to False if you don't want to run the script by default
+if __name__ == "__main__" and True: # Change True to False if you don't want to run the script by default
     print(Fore.YELLOW + f"Website started on port {port}\n" +Fore.WHITE)
     app.run(debug=True,port=port)
     
